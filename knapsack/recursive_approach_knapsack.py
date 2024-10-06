c13a4a783ee95af00072d3deed36a28ea3def057
Kaustubh Nimbalkar <75576508+nk1947-sudo@users.noreply.github.com>
2024-10-07 00:11:37 +0530

    Update recursive_approach_knapsack.py
    used dictionary, reduce time complexity

--- knapsack/recursive_approach_knapsack.py
+++ knapsack/recursive_approach_knapsack.py
@@ -1,51 +1,59 @@
-# To get an insight into naive recursive way to solve the Knapsack problem
-
-
-"""
-A shopkeeper has bags of wheat that each have different weights and different profits.
-eg.
-no_of_items 4
-profit 5 4 8 6
-weight 1 2 4 5
-max_weight 5
-Constraints:
-max_weight > 0
-profit[i] >= 0
-weight[i] >= 0
-Calculate the maximum profit that the shopkeeper can make given maxmum weight that can
-be carried.
-"""
-
-
-def knapsack(
-    weights: list, values: list, number_of_items: int, max_weight: int, index: int
-) -> int:
+def knapsack_memoized(weights: list, values: list, number_of_items: int, max_weight: int, index: int, memo: dict) -> int:
     """
-    Function description is as follows-
-    :param weights: Take a list of weights
-    :param values: Take a list of profits corresponding to the weights
-    :param number_of_items: number of items available to pick from
-    :param max_weight: Maximum weight that could be carried
-    :param index: the element we are looking at
-    :return: Maximum expected gain
-    >>> knapsack([1, 2, 4, 5], [5, 4, 8, 6], 4, 5, 0)
-    13
-    >>> knapsack([3 ,4 , 5], [10, 9 , 8], 3, 25, 0)
-    27
+    Optimized version using memoization to solve the knapsack problem.
+    :param weights: A list of weights.
+    :param values: A list of profits corresponding to the weights.
+    :param number_of_items: Total number of items.
+    :param max_weight: Maximum weight that can be carried.
+    :param index: The index of the item currently being considered.
+    :param memo: Dictionary for memoization to store previously computed results.
+    :return: Maximum profit that can be obtained.
     """
+    # Base case: No more items to process or no more capacity in the knapsack
     if index == number_of_items:
         return 0
-    ans1 = 0
-    ans2 = 0
-    ans1 = knapsack(weights, values, number_of_items, max_weight, index + 1)
+    
+    # Check if the solution to this subproblem is already computed
+    if (index, max_weight) in memo:
+        return memo[(index, max_weight)]
+    
+    # Option 1: Exclude the current item
+    exclude_current_item = knapsack_memoized(weights, values, number_of_items, max_weight, index + 1, memo)
+    
+    # Option 2: Include the current item (if its weight allows)
+    include_current_item = 0
     if weights[index] <= max_weight:
-        ans2 = values[index] + knapsack(
-            weights, values, number_of_items, max_weight - weights[index], index + 1
+        include_current_item = values[index] + knapsack_memoized(
+            weights, values, number_of_items, max_weight - weights[index], index + 1, memo
         )
-    return max(ans1, ans2)
+    
+    # Store the result in the memo table
+    result = max(exclude_current_item, include_current_item)
+    memo[(index, max_weight)] = result
+    
+    return result
+
+
+def knapsack(weights: list, values: list, max_weight: int) -> int:
+    """
+    Wrapper function for the memoized knapsack solver.
+    :param weights: A list of weights.
+    :param values: A list of profits corresponding to the weights.
+    :param max_weight: Maximum weight that can be carried.
+    :return: Maximum profit that can be obtained.
+    """
+    number_of_items = len(weights)
+    memo = {}
+    return knapsack_memoized(weights, values, number_of_items, max_weight, 0, memo)
 
 
 if __name__ == "__main__":
-    import doctest
+    weights = [1, 2, 4, 5]
+    values = [5, 4, 8, 6]
+    max_weight = 5
+    print(knapsack(weights, values, max_weight))  # Expected output: 13
 
-    doctest.testmod()
+    weights2 = [3, 4, 5]
+    values2 = [10, 9, 8]
+    max_weight2 = 25
+    print(knapsack(weights2, values2, max_weight2))  # Expected output: 27
